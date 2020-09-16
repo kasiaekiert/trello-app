e4446696cda7863f18d3691c74d5d44b97190208
kasiaekiert <kasia.ekiert@gmail.com>
2020-09-16 09:32:42 +0200

    list params

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,14 @@
 class ListsController < ApplicationController
+    before_action :find_board_list, only: [:edit, :update, :destroy] 
 
     def new
         @board = Board.find(params[:board_id])
-        @list = List.new
+        @list = @board.lists.new
     end
 
     def create
         @board = Board.find(params[:board_id])
-        @list = @board.lists.new
+        @list = @board.lists.create(create_params)
         if @list.save
             redirect_to board_path(@board), notice: 'List created'
         else
@@ -16,18 +17,31 @@ class ListsController < ApplicationController
     end
 
     def edit
-        @board = Board.find(params[:board_id])
-        @list = @board.lists.find_by(params[:id])
     end
 
     def update
-        @board = Board.find(params[:board_id])
-        @list = @board.lists.find_by(params[:id])
           if @list.update(name: params[:list][:name])
             redirect_to boards_path(@board), notice: 'List name changed!'
           else
             render :edit
           end
     end
+
+    def destroy
+        @list.destroy!
+        redirect_to boards_path(@board), notice: 'List deleyed'
+    end
+
+    private
+
+    def find_board_list
+        @board = Board.find(params[:board_id])
+        @list = @board.lists.find_by(params[:id])
+    end
+
+    def create_params
+        params.require(:list).permit(:name)
+    end
+
 
 end
